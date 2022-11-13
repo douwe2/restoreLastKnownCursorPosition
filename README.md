a90ed03db86a9dd4e1c9b6da8e3f66fd2332adcb
Douwe <temp2020@mailbox.org>
2022-11-13 13:34:29 +0100

    Updated files for publishing

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 
 While editing it keeps track of the lastest cursor position in your document. When you later open the document that cursor position will
 be restored.
+This primarly meant if you use code as a single document editor (ie notepad or nedit)
+When you press a mouse button in a code texteditor window the edito cursor is set to where you the mouse pointer was, in that case
+press ctrl+al+c to restore the cursor position to the cursor position the cursor was at when the document was previously opened.
 
 
 **Enjoy!**
--- src/extension.ts
+++ src/extension.ts
@@ -13,7 +13,7 @@ export function activate(context: vscode.ExtensionContext) {
 		  const offset = evt.textEditor.document.offsetAt(evt.selections[0].active)
 		  const file = evt.textEditor.document.fileName;
 		  context.globalState.update(file, offset)		  
-		  console.log(file,offset)
+		  //debug console.log(file,offset)
 	})
 
 	// when the document is opened again th texteditor is updated with the last known cursor
@@ -24,8 +24,27 @@ export function activate(context: vscode.ExtensionContext) {
 		const position = document.positionAt(offset!)
 		const validpos = document.validatePosition(position)
 		//debug console.log(file,offset)	  
+		context.globalState.update(file+"*",offset)
+		// debug console.log(file+"*",offset)
 		vscode.window.activeTextEditor!.selection=new vscode.Selection(validpos,validpos)
 	});
+
+    // The command has been defined in the package.json file
+    // Now provide the implementation of the command with  registerCommand
+    // The commandId parameter must match the command field in package.json
+    var disposable = vscode.commands.registerTextEditorCommand('extension.restoreLastKnownCursorPosition', (texteditor: vscode.TextEditor) => {
+        // The code you place here will be executed every time your command is executed
+		const file = texteditor.document.fileName;
+		const offset = context.globalState.get<number>(file+"*")
+		// debug console.log(file+"*",offset)
+		const position = texteditor.document.positionAt(offset!)
+		const validpos = texteditor.document.validatePosition(position)
+		//debug console.log(file,offset)	  		
+		texteditor.selection=new vscode.Selection(validpos,validpos)		
+    });
+
+    context.subscriptions.push(disposable);
+
 }
 
 // This method is called when your extension is deactivated
